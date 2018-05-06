1def8acce568b9df241dbf869dc9d2710faa7355
Santiago Romero <bonampa312@gmail.com>
2018-05-06 18:19:26 -0500

    Trying redirects in home page 2

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,7 @@ angular.module("DarkArmoryAPI", ['ngRoute'])
             .when("/", {
                 templateUrl: "home.html",
                 controller: "HomeController"
-            })/*
-            .when("/ds2/weapons", {
-                redirectTo: "/ds2/weapons"
             })
-            /*
-            .otherwise({
-                redirectTo: "/"
-            }) */
     })
     .controller("HomeController", function($scope) {
-        $scope.goToDS1Weapons = function () {
-            $location.path("/ds1/weapons")
-        }
     });
